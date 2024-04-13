3782d3be04acb9e4216298a900534c7e647b7b48
techie311 <54245244+techie311@users.noreply.github.com>
2024-04-13 16:00:55 +0800

    Create f2l.is-an.app.js
    page to download and stream files from telegram file to link bot

---
+++ domains/f2l.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Free file to link telegram bot',
+  domain: 'is-an.app',
+  subdomain: 'f2l',
+  owner: {
+    email: 'ahnaan01@gmail.com',
+  },
+  record: {
+    A: [
+      '52.221.248.223',
+    ],
+  },
+})
